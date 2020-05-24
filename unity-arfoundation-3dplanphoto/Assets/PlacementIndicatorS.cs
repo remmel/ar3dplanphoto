ed07db4134ff9ce2048655552fd2d3ac89bb7940
Remy Mellet <remy.meller@gmail.com>
2020-05-24 18:22:32 +0200

    init basic working project
    can pose cube and take screenshot

---
+++ unity-arfoundation-3dplanphoto/Assets/PlacementIndicatorS.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicatorS : MonoBehaviour
+{
+    private ARRaycastManager rayManager;
+    private GameObject visual;
+
+    void Start()
+    {
+        rayManager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;
+
+        visual.SetActive(false);
+    }
+
+    void Update()
+    {
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        if(hits.Count > 0) {
+            //placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+            Pose pose = hits[0].pose;
+            transform.position = pose.position;
+            transform.rotation = pose.rotation;
+
+            //if (!visual.activeInHierarchy) {
+                visual.SetActive(true);
+            //}
+        }
+    }
+}
---
+++ unity-arfoundation-3dplanphoto/Assets/TakePhotoS.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ToastPlugin;
+using UnityEngine;
+
+public class TakePhotoS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Take() {
+        //yield return new WaitForEndOfFrame();
+
+        Texture2D snap = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        snap.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        snap.Apply();
+        //System.IO.File.WriteAllBytes(filename, texture.EncodeToPNG());
+
+        string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+        string path = Application.persistentDataPath + "/Screenshot_" + timeStamp + ".jpg";
+        System.IO.File.WriteAllBytes(path, snap.EncodeToJPG());
+        Debug.Log("Screenshot saved in " + path);
+        ToastHelper.ShowToast("Screenshot saved in " + path);
+
+    }
+}
